0a26d939d52426aea65b107a5b73d943c4cd63b9
Raon Choi <h1choi@ucsd.edu>
2022-11-09 16:50:39 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,7 +68,7 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
-  if (localStorage.getItem("recipes" != null)) return localStorage.getItem("recipes");
+  if (localStorage.getItem("recipes") !== null) return localStorage.getItem("recipes");
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
@@ -79,11 +79,11 @@ async function getRecipes() {
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
-  return new Promise(async (callback) => { //A3
+  return new Promise(async callback => { //A3
       for (let i = 0; i < RECIPE_URLS.length; i++) { //A4
         try { //A5
-          const fetch = await fetch(RECIPE_URLS[i]); //A6
-          const response = await fetch.json(); //A7
+          const fet = await fetch(RECIPE_URLS[i]); //A6
+          const response = await fet.json(); //A7
           recipe.push(response); //A8
           if (recipe.length == RECIPE_URLS.length) saveRecipesToStorage();  //A9
           Promise.resolve(recipe);

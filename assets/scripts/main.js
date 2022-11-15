e9d40e5d4160d867fa56ee10f731aa753cf41fda
Raon Choi <h1choi@ucsd.edu>
2022-11-14 22:25:57 -0800

    Update sw.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,20 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', (event) => {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log("Service worker registration succeeded:", registration);
+        })
+        .catch((error) => {
+          console.error(`Service worker registration failed: ${error}`);
+        });
+    });
+  } else {
+    console.log("Service workers are not supported.");
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
